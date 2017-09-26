bd31c707fcf929b05d445699746f97e145654903
Jacek Podkanski <ruby.object@googlemail.com>
2017-09-26 14:20:04 +0100

    gui seems to work

--- src/clomimap/core.clj
+++ src/clomimap/core.clj
@@ -1,14 +1,10 @@
 (ns clomimap.core
-  (:use seesaw.core
-   seesaw.graphics
-   seesaw.color
-   seesaw.dev                      ;if you comment it out you can use (show-options)
-   seesaw.chooser
-   [clojure.pprint :only [cl-format]])
+  (:use seesaw.core))
 
-  (:gen-class))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn -main [& args]
+  (invoke-later
+   (-> (frame :title "Hello",
+              :content "Hello, Seesaw",
+              :on-close :exit)
+       pack!
+       show!)))
